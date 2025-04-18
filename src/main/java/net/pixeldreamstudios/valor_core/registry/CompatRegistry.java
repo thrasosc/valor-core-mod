c12c165f372308b7f1a0d9c55c0ee80d14238722
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-04-18 18:48:22 +0200

    add Scorchful and Frostiful compatibility

---
+++ src/main/java/net/pixeldreamstudios/valor_core/registry/CompatRegistry.java
@@ -0,0 +1,33 @@
+package net.pixeldreamstudios.valor_core.registry;
+
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.resources.ResourceLocation;
+import net.pixeldreamstudios.valor_core.ValorCore;
+
+public class CompatRegistry {
+  private static void registerScorchfulCompat() {
+    if (FabricLoader.getInstance().isModLoaded("scorchful")) {
+      ResourceManagerHelper.registerBuiltinResourcePack(
+          ResourceLocation.fromNamespaceAndPath(ValorCore.MOD_ID, "scorchful_levelz"),
+          FabricLoader.getInstance().getModContainer(ValorCore.MOD_ID).orElseThrow(),
+          ResourcePackActivationType.DEFAULT_ENABLED);
+    }
+  }
+
+  private static void registerFrostifulCompat() {
+    if (FabricLoader.getInstance().isModLoaded("frostiful")) {
+      ResourceManagerHelper.registerBuiltinResourcePack(
+          ResourceLocation.fromNamespaceAndPath(ValorCore.MOD_ID, "frostiful_levelz"),
+          FabricLoader.getInstance().getModContainer(ValorCore.MOD_ID).orElseThrow(),
+          ResourcePackActivationType.DEFAULT_ENABLED);
+    }
+  }
+
+  public static void init() {
+    ValorCore.LOGGER.info("Registering compatibilities");
+    registerScorchfulCompat();
+    registerFrostifulCompat();
+  }
+}
