5a413f620529ee1dea266961feb446dfdbf3640d
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-04-18 18:48:47 +0200

    move registry initialisation to correct place

--- src/main/java/net/pixeldreamstudios/valor_core/ValorCore.java
+++ src/main/java/net/pixeldreamstudios/valor_core/ValorCore.java
@@ -2,18 +2,22 @@ package net.pixeldreamstudios.valor_core;
 
 import net.fabricmc.api.ModInitializer;
 import net.pixeldreamstudios.valor_core.registry.CompatRegistry;
+import net.pixeldreamstudios.valor_core.registry.CreativeModeTabRegistry;
+import net.pixeldreamstudios.valor_core.registry.ItemRegistry;
 import net.pixeldreamstudios.valor_core.util.ClientSetup;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ValorCore implements ModInitializer {
-    public static final String MOD_ID = "valor_core";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+  public static final String MOD_ID = "valor_core";
+  public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-    @Override
-    public void onInitialize() {
-        LOGGER.info("Initialising Valor Core");
-        ClientSetup.createGuideBookButton();
-    }
+  @Override
+  public void onInitialize() {
+    LOGGER.info("Initialising Valor Core");
+    ClientSetup.createGuideBookButton();
+    CreativeModeTabRegistry.init();
+    ItemRegistry.init();
     CompatRegistry.init();
+  }
 }
